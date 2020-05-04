23050b0cd17ab37f53265819811c8cd3cc1e30a3
Max Keeble <meckeeble@hotmail.co.uk>
2020-05-04 16:58:01 +0100

    Update Impale.cs

--- Impale.cs
+++ Impale.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +9,7 @@ public class Impale : MonoBehaviour
     public Rigidbody body;
     public Transform stickPoint;
     public Collider col;
+    public Vector3 offset;
 
     public GameObject [] parts;
     // Start is called before the first frame update
@@ -18,12 +19,13 @@ public class Impale : MonoBehaviour
     body = Human.transform.GetChild(0).GetChild(5).GetComponent<Rigidbody>();
     stickPoint = this.gameObject.transform.GetChild(1).transform;
     col = GetComponent<Collider>();
+    offset = new Vector3(0,0,-0.2f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //stickPoint.localPosition = Vector3.zero;
     }
     void OnTriggerEnter(Collider collision){
         if (collision.gameObject == Human){
@@ -32,9 +34,9 @@ public class Impale : MonoBehaviour
         Human.GetComponent<RagDoll>().DoRagdoll(true);
         col.enabled=false;
         collision.gameObject.transform.parent = stickPoint;
+        stickPoint.localPosition = Vector3.zero;
         body.constraints = RigidbodyConstraints.FreezeAll;
 
-        
                        }
     }
 }
